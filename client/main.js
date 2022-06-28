a28b342fe3bb05d7156e466cfaa2ebbfbcaf62ad
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-06-28 03:05:21 +0200

    initial commit

--- client/main.js
+++ client/main.js
@@ -1,6 +1,13 @@
-import './style.css'
+import Alpine from 'alpinejs';
+import { Login } from './login';
+import Fun from './fun';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+window.Alpine = Alpine;
+
+
+Alpine.data('login', Login);
+Alpine.data('fun', Fun);
+
+Alpine.start();
+
+
